6716a22bb7f6037be9a84a96ae36c6d451879f90
SivinX11 <sivin2552000@gmail.com>
2021-05-07 13:23:01 +0530

    Files

--- app.py
+++ app.py
@@ -86,6 +86,52 @@ def page_users():
 				st.success("User Removed")
 
 
+def page_users_root():
+	st.title("Users' Management")
+	option = st.selectbox('Select an action',('View Users', 'Add User', 'Remove A User'))
+	if(option=="View Users"):
+		users=pd.read_csv('users.csv',index_col=0)
+		st.table(users)
+	if(option=='Add User'):
+		st.header("Enter User Info:")
+		form = st.form(key="my-form")
+		name = form.text_input("Name")
+		user_id=form.text_input("User ID")
+		phone_number=form.text_input("Phone Number")
+		submit = form.form_submit_button('Submit')
+		if submit:
+			if(verify_userid(user_id)):
+				st.error("User ID exists")
+			else:
+				users=pd.read_csv('users.csv',index_col=0)
+				data_to_add=[{'Name':name,'User ID':user_id,'Phone':phone_number}]
+				tempdf=pd.DataFrame(data_to_add)
+				users=users.append(tempdf, ignore_index=True)
+				users.index+=1
+				users.to_csv('users.csv')
+				st.success("Added User")
+	if(option=='Remove A User'):
+		st.header("Enter User ID to remove a user:")
+		form = st.form(key="my-form")
+		user_id=form.text_input("User ID")
+		submit = form.form_submit_button('Submit')
+		if submit:
+			if(not(verify_userid(user_id))):
+				st.error("User ID does not exist")
+			else:
+				data = pd.read_csv("users.csv", index_col =0)
+				sets=[]
+				k=1
+				for i in (data["User ID"]):
+					if(str(i)==user_id):
+						sets.append(k)
+					k+=1
+				data.drop(sets, inplace = True)
+				data=data.reset_index(drop=True)
+				data.index+=1
+				data.to_csv('users.csv')
+				st.success("User Removed")
+
 def page_drivers():
 	st.title("Drivers' Management")
 	option = st.selectbox('Select an action',('View Drivers', 'Add Driver', 'Remove A Driver'))
@@ -173,6 +219,45 @@ def page_add_bookings():
 			st.success("Added Booking")
 
 
+def page_add_bookings_unrooted():
+	st.title("Create New Booking")
+	st.header("Enter booking Info:")
+	form = st.form(key="my-form")
+	emp_id=form.text_input("Employee ID")
+	user_id=form.text_input("User ID")
+	date=form.date_input("Booking Date")
+	now = datetime.now()
+	booking_id=now.strftime("%S%M%H%d%m")
+	submit = form.form_submit_button('Submit')
+	if submit:
+		if(not(verify_empid(emp_id))):
+			st.error("Employee ID does not exist")
+		elif(not(verify_userid(user_id))):
+			st.error("User ID does not exist")
+		else:
+			drivers = pd.read_csv("drivers.csv", index_col =0)
+			users=pd.read_csv('users.csv',index_col=0)
+			bookings=pd.read_csv('bookings.csv',index_col=0)
+			user_sets=[]
+			k=1
+			for i in (users["User ID"]):
+				if(str(i)==user_id):
+					user_sets.append(k)
+				k+=1
+			emp_sets=[]
+			k=1
+			for i in (drivers["Employee ID"]):
+				if(str(i)==emp_id):
+					emp_sets.append(k)
+				k+=1
+			data = [{'User Name':users['Name'][user_sets[0]],'User ID':users['User ID'][user_sets[0]],'Phone':users['Phone'][user_sets[0]],"Driver's Name":drivers['Name'][emp_sets[0]],"Employee ID":drivers['Employee ID'][emp_sets[0]],"Type of Vehicle":drivers['Type of Vehicle'][emp_sets[0]],"Registration":drivers['Registration'][emp_sets[0]],'Date':date,'Booking ID':booking_id}]
+			tempdf=pd.DataFrame(data)
+			bookings=bookings.append(tempdf, ignore_index=True)
+			bookings.index+=1
+			bookings.to_csv('bookings.csv')
+			st.success("Added Booking")
+
+
 
 def page_clear():
 	st.title("Clear History")
@@ -197,7 +282,28 @@ def page_clear():
 			data.to_csv('bookings.csv')
 			st.success("Booking Removed")
 
-
+def page_clear_rooted():
+	st.title("Clear History")
+	st.header("Enter the Booking ID to remove a booking:")
+	form = st.form(key="my-form")
+	book_id=form.text_input("Booking ID")
+	submit = form.form_submit_button('Submit')
+	if submit:
+		if(not(verify_bookid(book_id))):
+			st.error("Booking ID does not exist")
+		else:
+			data = pd.read_csv("bookings.csv", index_col =0)
+			sets=[]
+			k=1
+			for i in (data["Booking ID"]):
+				if(str(i)==book_id):
+					sets.append(k)
+				k+=1
+			data.drop(sets, inplace = True)
+			data=data.reset_index(drop=True)
+			data.index+=1
+			data.to_csv('bookings.csv')
+			st.success("Booking Removed")
 
 def main():
 	st.sidebar.title("Navigation")
